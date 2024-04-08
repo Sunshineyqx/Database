6334deb948a497145f9edb3454df250ba7c99ff7
sunshine <3275316624@qq.com>
2024-04-08 19:35:37 +0800

    project 0: task2 并发键值存储ok

--- src/primer/trie_store.cpp
+++ src/primer/trie_store.cpp
@@ -1,4 +1,5 @@
 #include "primer/trie_store.h"
+#include <optional>
 #include "common/exception.h"
 
 namespace bustub {
@@ -8,23 +9,37 @@ auto TrieStore::Get(std::string_view key) -> std::optional<ValueGuard<T>> {
   // Pseudo-code:
   // (1) Take the root lock, get the root, and release the root lock. Don't lookup the value in the
   //     trie while holding the root lock.
+  root_lock_.lock();
+  auto root = root_;
+  root_lock_.unlock();
   // (2) Lookup the value in the trie.
+  auto res = root.Get<T>(key);
   // (3) If the value is found, return a ValueGuard object that holds a reference to the value and the
   //     root. Otherwise, return std::nullopt.
-  throw NotImplementedException("TrieStore::Get is not implemented.");
+  if(!res){
+    return std::nullopt;
+  }
+  return ValueGuard<T>(root, *res);
+  //throw NotImplementedException("TrieStore::Get is not implemented.");
 }
 
 template <class T>
 void TrieStore::Put(std::string_view key, T value) {
   // You will need to ensure there is only one writer at a time. Think of how you can achieve this.
   // The logic should be somehow similar to `TrieStore::Get`.
-  throw NotImplementedException("TrieStore::Put is not implemented.");
+  //throw NotImplementedException("TrieStore::Put is not implemented.");
+  write_lock_.lock();
+  root_ = root_.Put(key, std::move(value));
+  write_lock_.unlock();
 }
 
 void TrieStore::Remove(std::string_view key) {
   // You will need to ensure there is only one writer at a time. Think of how you can achieve this.
   // The logic should be somehow similar to `TrieStore::Get`.
-  throw NotImplementedException("TrieStore::Remove is not implemented.");
+  //throw NotImplementedException("TrieStore::Remove is not implemented.");
+  write_lock_.lock();
+  root_ = root_.Remove(key);
+  write_lock_.unlock();
 }
 
 // Below are explicit instantiation of template functions.
